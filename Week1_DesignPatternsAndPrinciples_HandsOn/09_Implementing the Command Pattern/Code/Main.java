cd43d25ee1d8ac29ee3326d185d34ed1d46c8167
tharun637 <vtharun9751@gmail.com>
2025-06-22 17:43:47 +0530

    Add files via upload

---
+++ Week1_DesignPatternsAndPrinciples_HandsOn/09_Implementing the Command Pattern/Code/Main.java
@@ -0,0 +1,19 @@
+public class Main {
+    public static void main(String[] args) {
+        Light livingRoomLight = new Light();
+
+        Command lightOn = new LightOnCommand(livingRoomLight);
+        Command lightOff = new LightOffCommand(livingRoomLight);
+
+        RemoteControl remote = new RemoteControl();
+
+        System.out.println();
+
+        remote.setCommand(lightOn);
+        remote.pressButton();
+
+        remote.setCommand(lightOff);
+        remote.pressButton();
+    }
+}
+
