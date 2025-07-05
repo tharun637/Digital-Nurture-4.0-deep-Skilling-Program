c36e290ed5e4b4c2a72241a015747e8d4aa60396
tharun637 <vtharun9751@gmail.com>
2025-07-05 22:34:48 +0530

    Exercise Completed

--- Week3_Spring Data JPA with Hibernate/02_Spring-data-JPA-HandsOn/03_Create payroll tables and bean mapping/Code/orm-learn/src/main/java/com/cognizant/orm_learn/OrmLearnApplication.java
+++ Week3_Spring Data JPA with Hibernate/02_Spring-data-JPA-HandsOn/03_Create payroll tables and bean mapping/Code/orm-learn/src/main/java/com/cognizant/orm_learn/OrmLearnApplication.java
@@ -1,13 +1,35 @@
 package com.cognizant.orm_learn;
 
+import com.cognizant.orm_learn.model.Employee;
+import com.cognizant.orm_learn.repository.EmployeeRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.transaction.annotation.Transactional;
+
+import java.util.List;
 
 @SpringBootApplication
-public class OrmLearnApplication {
+public class OrmLearnApplication implements CommandLineRunner {
+
+	@Autowired
+	private EmployeeRepository employeeRepository;
 
 	public static void main(String[] args) {
 		SpringApplication.run(OrmLearnApplication.class, args);
 	}
 
+	@Override
+	@Transactional
+	public void run(String... args) {
+		List<Employee> employees = employeeRepository.findAll();
+		for (Employee e : employees) {
+			System.out.println("==================================");
+			System.out.println("Employee: " + e);
+			System.out.println("Department: " + e.getDepartment());
+			System.out.println("Skills: " + e.getSkills());
+			System.out.println("==================================");
+		}
+	}
 }
