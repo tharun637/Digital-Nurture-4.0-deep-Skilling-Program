1679aec286a010aeee4fe4b0cf866a8db63b8961
tharun637 <vtharun9751@gmail.com>
2025-07-09 22:25:51 +0530

    Spring Rest

---
+++ Week4_Spring REST using Spring Boot/01_Spring-Rest-HandsOn/Code/spring-learn/src/main/java/com/cognizant/spring_learn/SpringLearnApplication.java
@@ -0,0 +1,15 @@
+package com.cognizant.spring_learn;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringLearnApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringLearnApplication.class, args);
+		
+		System.out.println("Executing the Spring Boot Application...");
+	}
+
+}
